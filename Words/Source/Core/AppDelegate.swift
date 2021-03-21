0ccbe3f4e60afdc89c4ff38df8206ce2accdbcb5
Rafał Grodziński <rafal@unalignedbyte.com>
2021-03-21 16:59:37 +0900

    Added google firebase

--- Words/Source/Core/AppDelegate.swift
+++ Words/Source/Core/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 
 class AppDelegate: UIResponder, UIApplicationDelegate
@@ -16,10 +17,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
 
+        setupAnalytics()
         printDebugInfo()
 
         return true
     }
+    
+    fileprivate func setupAnalytics()
+    {
+        FirebaseApp.configure()
+    }
+    
 
     fileprivate func printDebugInfo()
     {
